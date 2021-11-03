06966c77f48a6c5ce1adcf35273cced3f1d86b79
croc31 <clauberogerioo@gmail.com>
2021-11-02 23:06:23 -0300

    separar linhas e colunas
    Adição de funções que separam as linhas e as colunas das matrizes

--- sequencial.cpp
+++ sequencial.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fstream>
+#include <map>
+#include <vector>
+//#include <utility>
 
 using namespace std;
 
@@ -13,6 +16,69 @@ void separaDimencoes(size_t *n, size_t *m, std::ifstream *stream)
     *n = stoi(stringAuxiliar);
     *stream >> stringAuxiliar;
     *m = stoi(stringAuxiliar);
+
+    /*stream->seekg(0, stream->end);
+    size_t tamanho = stream->tellg();
+    stream->seekg(0, stream->beg);
+
+    (*stream).seekg(tamanho / 2);
+    (*stream).ignore(256, 'c');
+    *stream >> stringAuxiliar;
+    cout << stringAuxiliar << endl;*/
+}
+
+void pegaLinha(const size_t n, const size_t *m, vector<float> *linha, const map<string, string> *buffer)
+{
+
+    for (size_t j = 0; j < *m; j++)
+    {
+        string auxiliar = "c";
+        auxiliar += to_string(n);
+        auxiliar += to_string(j);
+
+        //cout << "erro aqui" << endl;
+        linha->push_back(stof((buffer->at(auxiliar))));
+    }
+}
+
+void pegaColuna(const size_t *n, const size_t m, vector<float> *coluna, const map<string, string> *buffer)
+{
+
+    for (size_t j = 0; j < *n; j++)
+    {
+        string auxiliar = "c";
+        auxiliar += to_string(j);
+        auxiliar += to_string(m);
+
+        //cout << "erro aqui" << endl;
+        coluna->push_back(stof((buffer->at(auxiliar))));
+    }
+}
+
+void fazBuffer(size_t *n1, size_t *m1, size_t *n2, size_t *m2, std::ifstream *matrizColuna, std::ifstream *matrizLinha)
+{
+    map<string, string> buffer;
+    vector<float> linha, coluna;
+    while (!matrizLinha->eof())
+    {
+        string chave, valor;
+        *matrizLinha >> chave;
+        *matrizLinha >> valor;
+        buffer.insert(pair<string, string>(chave, valor));
+    }
+    pegaLinha(1, m1, &linha, &buffer);
+    pegaColuna(n1, 1, &coluna, &buffer);
+    // cout << "linha" << endl;
+    // for (auto &e : linha)
+    // {
+    //     cout << e << endl;
+    // }
+    // cout << '\n'
+    //      << "Coluna" << endl;
+    // for (auto &e : coluna)
+    // {
+    //     cout << e << endl;
+    // }
 }
 
 int main(int argc, char const *argv[])
@@ -51,7 +117,13 @@ int main(int argc, char const *argv[])
            *m2 = new size_t;
     separaDimencoes(n1, m1, &matriz1);
     separaDimencoes(n2, m2, &matriz2);
+    if (*m1 != *n2)
+    {
+        std::cerr << "O número de colunas da matriz1 deve ser igual ao número de linhas da matriz 2 " << std::endl;
+        exit(3);
+    }
 
+    fazBuffer(n1, m1, n2, m2, &matriz1, &matriz2);
     //liberando memória alocada
     delete n1;
     delete n2;
